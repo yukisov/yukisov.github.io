07bd9356265146fca899b7ec4c1beb7fadc42425
yuki <yukisov@gmail.com>
2015-09-02 21:35:28 +0900

    Remove non-used lines in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-var webpack = require('webpack');
 module.exports = {
   // configuration
   module: {
@@ -13,7 +12,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel'
+        loader: 'babel-loader'
       }
     ]
   },
